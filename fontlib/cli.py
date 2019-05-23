6a967c47b54025e5e63af185c8bf4b97abdc281b
Markus Heiser <markus.heiser@darmarit.de>
2019-05-23 19:00:20 +0200

    cli: add 'css-parse' command / parse ``@font-face`` rules from <url>
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- fontlib/cli.py
+++ fontlib/cli.py
@@ -7,6 +7,33 @@ import sys
 from fspath import CLI
 from fspath.sui import SimpleUserInterface
 
+from .fontstack import FontStack
+
+# ==============================================================================
+def _cli_parse_css(args):
+# ==============================================================================
+
+    u"""
+    Parse ``@font-face`` rules from <url>.
+
+    Load CSS (stylesheet) from <url> and filter ``@font-face`` rules.  E.g. google
+    fonts API 'https://fonts.googleapis.com/css?family=Cute+Font|Roboto+Slab' or
+    built-in *dejavu* fonts from url 'file:./fontlib/files/dejavu/dejavu.css'.
+    """
+
+    cli = args.CLI
+    cli.UI.echo("load css from url: %s" % args.url)
+
+    fs = FontStack()
+    fs.load_css(args.url)
+
+    cli.UI.rst_table(
+        fs.stack.values()
+        # <col-title>, <format sting>, <attribute name>
+        , ("Name",     "%-50s",        "name")
+        , ("URL",      "%-90s",        "url") )
+
+
 # ==============================================================================
 def main():
 # ==============================================================================
@@ -17,7 +44,12 @@ def main():
     cli    = CLI(description=main.__doc__)
     cli.UI = SimpleUserInterface(cli=cli)
 
-    # ...
+    css_parse = cli.addCMDParser(_cli_parse_css, cmdName='css-parse')
+    css_parse.add_argument(
+        "url"
+        , type = str
+        , help = "<url> of ccs to parse"
+        )
 
     # run ...
     cli()
