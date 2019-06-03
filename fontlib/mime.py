63ad76d316a6d72bfd3144ee6279033c64b91de4
Markus Heiser <markus.heiser@darmarit.de>
2019-06-03 14:36:56 +0200

    fontlib.mime: restructuring mime-type into FontlibMimeTypes
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- fontlib/mime.py
+++ fontlib/mime.py
@@ -13,9 +13,37 @@ log = logging.getLogger(__name__)
 
 MIMETYPES_FNAME = fspath.FSPath(__file__).DIRNAME / 'mime.types'
 
+class FontlibMimeTypes(mimetypes.MimeTypes):
+    """
+    A class (:py:class:`mimetypes.MimeTypes` ) with (only) mime types from ``fontlib/mime.types``
+    """
+    def __init__(self):
+        self.encodings_map = {}
+        self.suffix_map = {}
+        self.types_map = ({}, {})
+        self.types_map_inv = ({}, {})
+        log.debug("FontlibMimeTypes: load mime.types from: %s", MIMETYPES_FNAME)
+        with open(MIMETYPES_FNAME) as f:
+            self.readfp(f, True)
+
+    def map_types(self):
+        """Return tuples with suffix and mime type.
+
+        Optional `strict' argument when False adds a bunch of commonly found,
+        but non-standard types.
+
+        :return:
+
+        ``[]('.ttf', 'font/ttf'), ('.woff2', 'font/woff2'), ...]
+        """
+        return self.types_map[True].items()
+
 def add_types():
-    """Add ``fontlib/mime.types`` to :py:mod:`mimetypes` """
-    log.debug("load mime.types from: %s", MIMETYPES_FNAME)
-    mtypes = mimetypes.read_mime_types(MIMETYPES_FNAME)
-    for ext, mtype in mtypes.items():
+    """Add mime types from ``fontlib/mime.types`` to :py:mod:`mimetypes`
+
+    .. literalinclude:: ../fontlib/mime.types
+
+    """
+    for ext, mtype in FontlibMimeTypes().map_types():
+        log.debug("mime: add %s (%s)", mtype, ext)
         mimetypes.add_type(mtype, ext)
