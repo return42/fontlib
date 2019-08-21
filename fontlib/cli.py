7f24bcd72bafc88f14660aef55b9ca41c1c733ce
Markus Heiser <markus.heiser@darmarit.de>
2019-08-21 19:32:03 +0200

    cli: add config (.INI) to command line
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- fontlib/cli.py
+++ fontlib/cli.py
@@ -15,64 +15,114 @@ from .fontstack import FontStack
 from .fontstack import get_stack
 from .log import cfg as log_cfg
 from .mime import add_types
+from .config import Config
+
 log = logging.getLogger(__name__)
 
-# ==============================================================================
-def _cli_parse_css(args):
-# ==============================================================================
+UNKNOWN = object()
+CONFIG = Config()
+USER_INI = FSPath("~/.fontlib.ini")
 
-    u"""
-    Parse ``@font-face`` rules from <url>.
+# pylint: disable=bad-continuation
+CMDARG_TO_CFG = {
+    # cli-argument    : (cfg-section, cfg-option) # see ./config.ini
+    'builtins'        : ('fontstack', 'builtin fonts')
+    , 'epfonts'       : ('fontstack', 'entry points')
+    , 'google'        : ('google fonts', 'fonts')
+}
+# pylint: enable=bad-continuation
 
-    Load CSS (stylesheet) from <url> and filter ``@font-face`` rules.  E.g. google
-    fonts API 'https://fonts.googleapis.com/css?family=Cute+Font|Roboto+Slab' or
-    built-in *dejavu* fonts from url 'file:./fontlib/files/dejavu/dejavu.css'.
+def main():
+    """Tools from the fontlib library."""
+    dictConfig(log_cfg)
+    add_types()
+
+    cli = CLI(description=main.__doc__)
+    cli.UI = SimpleUserInterface(cli=cli)
+
+    # config ...
+
+    cli.add_argument(
+        '-c', '--config'
+        , help = 'specify config file'
+        , dest = 'config'
+        , type = FSPath
+        , default = USER_INI
+        , nargs = '?'
+        , metavar = 'INI-FILE' )
+
+    # cmd: list ...
+
+    list_fonts = cli.addCMDParser(cli_list_fonts, cmdName='list')
+    add_fontstack_options(list_fonts)
+
+    # cmd: css-parse
+
+    css_parse = cli.addCMDParser(cli_parse_css, cmdName='css-parse')
+    css_parse.add_argument(
+        "url"
+        , type = str
+        , help = "URL of stylesheet (CSS) to parse"
+    )
+
+    download_family = cli.addCMDParser(cli_download_family, cmdName='download')
+    add_fontstack_options(download_family)
+    download_family .add_argument(
+        "dest"
+        , type = FSPath
+        , help = "Folder where the download will be placed."
+    )
+    download_family .add_argument(
+        "family"
+        , type = str
+        , nargs = '+'
+        , help = (
+            "the font name, the value of CSS font-family property."
+            "E.g.  'Roboto Slab' 'DejaVu Sans Mono'")
+    )
+
+    # run ...
+    cli()
+
+
+def cli_list_fonts(args):
+    """List fonts from *builtins*, *fonts.googleapis.com* and other resources.
+
+    Option --google add fonts from google fonts infrastructure.  Select font
+    families from:
+
+        https://fonts.google.com
+
+    Option --ep-fonts add fonts from entry points, see:
+
+        https://pypi.org/project/fonts
+
+    To install fonts from the *python fonts project* e.g. run::
+
+        $ pip install --user \\
+              font-amatic-sc font-caladea font-font-awesome \\
+              font-fredoka-one font-hanken-grotesk font-intuitive \\
+              font-source-sans-pro font-source-serif-pro
+
     """
+    init_cfg(args)
 
     cli = args.CLI
-    cli.UI.echo("load css from url: %s" % args.url)
-
-    font_stack = FontStack()
-    font_stack.load_css(args.url)
-
-    cli.UI.rst_table(
-        font_stack.stack.values()
-        # <col-title>, <format sting>, <attribute name>
-        , ("Name",     "%-50s",        "font_name")
-        , ("URL",      "%-90s",        "url") )
-
-# ==============================================================================
-def _cli_list_fonts(args):
-# ==============================================================================
-
-    u"""List fonts from *builtins* and fonts.googleapis.com
-
-    Option --google list fonts from google fonts infrastructure.  See screenshot
-    at:
-
-        https://raw.githubusercontent.com/return42/fontlib/master/docs/fonts_google_com.png
-
-    Select your fonts on https://fonts.google.com (use the + button) and from
-    the *Families Selected* menu in the right bottom, use the bold marked
-    string, right to the 'family=' parameter.
-    """
-
-    cli = args.CLI
-    font_stack = _get_fontstack_from_options(args)
-    cache = font_stack.cache
+    stack = get_stack(CONFIG)
 
     def table_rows():
 
-        for font in font_stack.list_fonts():
+        for font in stack.list_fonts():
 
             closest = font.origin
-            blob_state = cache.url_state(font.origin)
+            blob_state = stack.cache.url_state(font.origin)
             if blob_state == 'cached':
-                closest = cache.fname_by_url(font.origin)
+                closest = stack.cache.fname_by_url(font.origin)
 
             yield dict(
                 ID = font.ID
                 , font_name = font.font_name
+                , format = font.format
                 , origin = font.origin
                 , blob_state = blob_state
                 , closest = closest
@@ -83,19 +133,42 @@ def _cli_list_fonts(args):
         # <col-title>,      <format sting>, <attribute name>
         , ("cache sate",    "%-10s",        "blob_state")
         , ("name",          "%-40s",        "font_name")
-        , ("ID",            "%-22s",        "ID")
+        , ("format",        "%-10s",        "format")
+        , ("font ID",       "%-22s",        "ID")
         , ("location",      "%-90s",        "closest") )
 
-# ==============================================================================
-def _cli_download_family(args):
-# ==============================================================================
 
-    u"""
-    Download font-family <family> to folder <dest>.
+def cli_parse_css(args):
+    """Parse ``@font-face`` rules from <url>.
+
+    Load CSS (stylesheet) from <url> and filter ``@font-face`` rules.  E.g. google
+    fonts API 'https://fonts.googleapis.com/css?family=Cute+Font|Roboto+Slab' or
+    built-in *dejavu* fonts from url 'file:./fontlib/files/dejavu/dejavu.css'.
+
     """
+    init_cfg(args)
+    cli = args.CLI
+    cli.UI.echo("load css from url: %s" % args.url)
+
+    font_stack = FontStack()
+    font_stack.load_css(args.url)
+
+    cli.UI.rst_table(
+        font_stack.stack.values()
+        # <col-title>, <format sting>, <attribute name>
+        , ("name",          "%-40s",        "font_name")
+        , ("format",        "%-10s",        "format")
+        , ("font ID",       "%-22s",        "ID")
+        , ("URL",           "%-90s",        "origin") )
+
+
+def cli_download_family(args):
+    """Download font-family <family> into folder <dest>."""
+
+    init_cfg(args)
 
     cli = args.CLI
-    font_stack = _get_fontstack_from_options(args)
+    stack = get_stack(CONFIG)
 
     if args.dest.EXISTS:
         cli.UI.echo("INFO: use existing folder %s" % args.dest)
@@ -107,7 +180,7 @@ def _cli_download_family(args):
     for font_family in args.family:
         c = 0
 
-        for font in font_stack.list_fonts(font_family):
+        for font in stack.list_fonts(font_family):
             url = urlparse(font.origin)
             dest_file = args.dest / FSPath(url.path).BASENAME
             if url.query:
@@ -117,7 +190,7 @@ def _cli_download_family(args):
 
             cli.UI.echo("[%s]: download %s from %s" % (
                 font.font_name, dest_file, font.origin))
-            font_stack.save_font(font, dest_file)
+            stack.save_font(font, dest_file)
             c += 1
         if c == 0:
             cli.UI.echo("ERROR: unknow font-family: %s" % font_family)
@@ -125,80 +198,60 @@ def _cli_download_family(args):
 
     cli.UI.echo("download %s files into %s" % (count, args.dest))
 
-
 # ==============================================================================
-# common comand line arguments
+# helper ...
 # ==============================================================================
 
-def _get_fontstack_from_options(args):
-
-    if args.builtins:
-        font_stack = get_stack()
-    else:
-        font_stack = FontStack()
-    if args.google:
-        font_stack.load_css(
-            'https://fonts.googleapis.com/css?family=' + args.google
-        )
-    return font_stack
-
-def _set_fontstack_options(cmd):
-
-    cmd.add_argument(
-        "--google"
-        , type = str
-        , help = "fonts from fonts.googleapis.com / to supress use --google ''"
-        , default = 'Cute+Font|Roboto+Slab'
-        )
+def add_fontstack_options(cmd):
+    """Adds common fontstack options to comand"""
     cmd.add_argument(
         "--builtins"
-        , action = "store_false"
+        , dest = 'builtins'
+        , default = CONFIG.get(*CMDARG_TO_CFG['builtins'][:2])
+        , nargs = '?', type = str
         , help = "use builtin fonts"
         )
 
+    cmd.add_argument(
+        "--ep-fonts"
+        , dest = 'epfonts'
+        , default = CONFIG.get(*CMDARG_TO_CFG['epfonts'][:2])
+        , nargs = '?', type = str
+        , help = "use fonts from entry points"
+        )
+
+    cmd.add_argument(
+        "--google"
+        , dest = 'google'
+        , default = CONFIG.get(*CMDARG_TO_CFG['google'][:2])
+        , nargs = '?', type = str
+        , help = "use fonts from fonts.googleapis.com"
+        )
+
+
+def init_cfg(args):
+    """Update the :py:class:`.config.Config` object from command line arguments"""
+
+    # load INI files ...
+
+    if args.config != USER_INI and not args.config.EXPANDUSER.EXISTS:
+        raise args.Error(42, 'config file does not extsts: %s' % args.config)
+    CONFIG.read(args.config.EXPANDUSER)
+
+    # update config from command line ..
+
+    for arg_name, (cfg_sect, cfg_opt) in CMDARG_TO_CFG.items():
+        value = args.__dict__.get(arg_name, UNKNOWN)
+        if value is UNKNOWN:
+            continue
+        if value is None:
+            value = ''
+        log.debug("set %s/%s = %s", cfg_sect, cfg_opt, value)
+        CONFIG.set(cfg_sect, cfg_opt, value)
 
 # ==============================================================================
-def main():
+# call main ...
 # ==============================================================================
-
-    u"""
-    Tools from the fontlib library
-    """
-
-    dictConfig(log_cfg)
-    add_types()
-
-    cli    = CLI(description=main.__doc__)
-    cli.UI = SimpleUserInterface(cli=cli)
-
-    css_parse = cli.addCMDParser(_cli_parse_css, cmdName='css-parse')
-    css_parse.add_argument(
-        "url"
-        , type = str
-        , help = "<url> of ccs to parse"
-    )
-
-    list_fonts = cli.addCMDParser(_cli_list_fonts, cmdName='list')
-    _set_fontstack_options(list_fonts)
-
-    download_family = cli.addCMDParser(_cli_download_family, cmdName='download')
-    _set_fontstack_options(download_family)
-    download_family .add_argument(
-        "dest"
-        , type = FSPath
-        , help = "Folder where the download will be placed."
-    )
-    download_family .add_argument(
-        "family"
-        , type = str
-        , nargs = '+'
-        , help = (
-            "The font name, the value of CSS font-family property."
-            "E.g.  'Roboto Slab' 'DejaVu Sans Mono'")
-    )
-
-    # run ...
-    cli()
 
 if __name__ == '__main__':
     sys.exit(main())
